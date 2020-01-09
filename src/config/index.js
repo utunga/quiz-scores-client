2e2a8e4eb65a32c03cc901bffc2e425ab1d197f7
Miles Thompson <utunga@gmail.com>
2020-01-10 01:49:43 +1300

    update config

--- src/config/index.js
+++ src/config/index.js
@@ -5,10 +5,8 @@ export const isDev = Boolean(process.env.NODE_ENV !== "production");
 export default {
   // connection: process.env.VUE_APP_API_URL
   //   ? process.env.VUE_APP_API_URL
-
-  domain: !isDev ? "*.now.sh" : "localhost",
+  domain: !isDev ? "*.herokuapp.com" : "localhost",
   connection: !isDev
-    ? //? "https://ooda-loops-server.now.sh"
-      "https://ooda-loops-server.utunga.now.sh"
+    ? "https://faction-claims-api.herokuapp.com"
     : "http://localhost:3030"
 };

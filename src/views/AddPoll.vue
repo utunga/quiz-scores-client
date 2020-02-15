01dc4ce388cf1881f5d0fbb529ee8963acd517de
simonwinter <simon@dragonfly.co.nz>
2020-02-15 23:14:49 +1300

    mobile modal updates

--- src/views/AddPoll.vue
+++ src/views/AddPoll.vue
@@ -1,159 +1,161 @@
 <template>
-    <div>
-        <button class="button is-primary is-medium"
-            @click="isModalOpen = true"
-        >
-            Add quiz
-        </button>
-        <b-modal
-          :active.sync="isModalOpen"
-          :width="640"
-          :onCancel="reset">
-          <form action="" @submit="checkForm" novalidate>
-            <div class="modal-card">
-                <header class="modal-card-head">
-                    <p class="modal-card-head-title">New quiz</p>
-                </header>
-                <div class="modal-card-body">
-                    <div class="content">
-                        <b-field
-                          label="Select a date"
-                          custom-class="is-small"
-                          :type="{ 'is-danger': errors.date }"
-                          :message="{ 'A quiz has already been recorded for this date': errors.date }">
-                            <b-datepicker
-                                ref="date"
-                                placeholder="Choose quiz date"
-                                icon="calendar"
-                                v-model="date">
-                            </b-datepicker>
-                        </b-field>
-                        <b-field
-                          label="Score"
-                          :type="{ 'is-danger': errors.score }"
-                          :message="{ 'A score is required': errors.score }"
-                          custom-class="is-small">
-                            <b-select 
-                              ref="scored"
-                              placeholder="Select a score"
-                              size="is-large"
-                            >
-                              <option
-                                  v-for="option in scoreOptions"
-                                  :value="option"
-                                  :key="option">
-                                  {{ option }}
-                              </option>
-                          </b-select>
-                        </b-field>
+  <b-modal
+    :active="isModalOpen"
+    :width="640"
+    :onCancel="reset"
+    :has-modal-card="true"
+    @close="closeModal">
+    <form action="" @submit="checkForm" novalidate>
+      <div class="modal-card">
+          <header class="modal-card-head">
+              <p class="modal-card-head-title">New quiz</p>
+          </header>
+          <div class="modal-card-body">
+              <div class="content">
+                  <b-field
+                    label="Select a date"
+                    custom-class="is-small"
+                    :type="{ 'is-danger': errors.date }"
+                    :message="{ 'A quiz has already been recorded for this date': errors.date }">
+                      <b-datepicker
+                          ref="date"
+                          placeholder="Choose quiz date"
+                          icon="calendar"
+                          v-model="date">
+                      </b-datepicker>
+                  </b-field>
+                  <b-field
+                    label="Score"
+                    :type="{ 'is-danger': errors.score }"
+                    :message="{ 'A score is required': errors.score }"
+                    custom-class="is-small">
+                      <b-select 
+                        ref="scored"
+                        placeholder="Select a score"
+                        size="is-large"
+                      >
+                        <option
+                            v-for="option in scoreOptions"
+                            :value="option"
+                            :key="option">
+                            {{ option }}
+                        </option>
+                    </b-select>
+                  </b-field>
 
-                        <AddUser
-                          @usersGroupUpdated="updatedUsers"
-                          :users="sortedUsers()"
-                          :has-error="errors.users"
-                        />
-                    </div>
-                </div>
-                 <footer class="modal-card-foot">
-                    <input type="submit" value="save" class="button is-primary" />
-                </footer>
-            </div>
-          </form>
-        </b-modal>
-    </div>
+                  <AddUser
+                    @usersGroupUpdated="updatedUsers"
+                    :users="sortedUsers()"
+                    :has-error="errors.users"
+                  />
+              </div>
+          </div>
+            <footer class="modal-card-foot">
+              <input type="submit" value="save" class="button is-primary" />
+          </footer>
+      </div>
+    </form>
+  </b-modal>
 </template>
 
 <script>
-import UsersMixin from "@/mixins/UsersMixin";
-import PollsMixin from "@/mixins/PollsMixin";
-import AddUser from "@/components/AddUser";
-import _ from "lodash";
+import UsersMixin from '@/mixins/UsersMixin'
+import PollsMixin from '@/mixins/PollsMixin'
+import AddUser from '@/components/AddUser'
+import _ from 'lodash'
 
 const defaultErrors = {
-  score: false,
-  users: false,
-  date: false
-};
+    score: false,
+    users: false,
+    date: false,
+}
 
 export default {
-  mixins: [PollsMixin, UsersMixin],
-  components: { AddUser },
-  data() {
-    return {
-      date: new Date(),
-      isModalOpen: false,
-      usersGroup: [],
-      errors: defaultErrors,
-      scoreOptions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    };
-  },
-  methods: {
-    users() {
-      return this.listUsers;
+    mixins: [PollsMixin, UsersMixin],
+    components: { AddUser },
+    props: {
+        isModalOpen: Boolean,
     },
-    sortedUsers() {
-      return _.sortBy(this.users(), ["name"]);
+    data() {
+        return {
+            date: new Date(),
+            usersGroup: [],
+            errors: defaultErrors,
+            scoreOptions: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        }
     },
-    polls() {
-      return this.listPolls;
+    methods: {
+        users() {
+            return this.listUsers
+        },
+        sortedUsers() {
+            return _.sortBy(this.users(), ['name'])
+        },
+        polls() {
+            return this.listPolls
+        },
+        checkForm(e) {
+            e.preventDefault()
+            this.checkErrors()
+
+            if (!this.errors.users && !this.errors.score && !this.errors.date) {
+                this.save()
+            }
+        },
+        checkErrors() {
+            this.errors.users = this.usersGroup.length === 0 ? true : false
+            this.errors.score =
+                this.$refs.scored.selected === null ? true : false
+            this.errors.date = this.date === null ? true : false
+
+            if (!this.errors.date) {
+                const curr = new Date(this.date)
+
+                const exists = _.findIndex(this.polls(), function(value) {
+                    const d = new Date(value.date)
+                    const dstr = `${d.getDate()}${d.getMonth()}${d.getFullYear()}`
+                    const currStr = `${curr.getDate()}${curr.getMonth()}${curr.getFullYear()}`
+
+                    return dstr === currStr
+                })
+
+                this.errors.date = exists === -1 ? false : true
+            }
+        },
+        reset() {
+            this.usersGroup = []
+            this.errors.users = false
+            this.errors.score = false
+            this.errors.date = false
+
+            this.date = new Date()
+        },
+        save() {
+            const date = this.date.toISOString()
+            const score = this.$refs.scored.selected
+            const out_of = 10
+            const present = this.usersGroup
+            this.createPoll({
+                date,
+                score,
+                out_of,
+                present,
+            }).then(() => {
+                this.closeModal()
+                this.$buefy.toast.open({
+                    message: 'Poll has been recorded',
+                })
+
+                this.reset()
+            })
+        },
+        updatedUsers(users) {
+            this.usersGroup = users
+        },
+        closeModal() {
+            this.$emit('pollModalClose')
+            return false
+        },
     },
-    checkForm(e) {
-      e.preventDefault();
-      this.checkErrors();
-
-      if (!this.errors.users && !this.errors.score && !this.errors.date) {
-        this.save();
-      }
-    },
-    checkErrors() {
-      this.errors.users = this.usersGroup.length === 0 ? true : false;
-      this.errors.score = this.$refs.scored.selected === null ? true : false;
-      this.errors.date = this.date === null ? true : false;
-
-      if (!this.errors.date) {
-        const curr = new Date(this.date);
-
-        const exists = _.findIndex(this.polls(), function(value) {
-          const d = new Date(value.date);
-          const dstr = `${d.getDate()}${d.getMonth()}${d.getFullYear()}`;
-          const currStr = `${curr.getDate()}${curr.getMonth()}${curr.getFullYear()}`;
-
-          return dstr === currStr;
-        });
-
-        this.errors.date = exists === -1 ? false : true;
-      }
-    },
-    reset() {
-      this.usersGroup = [];
-      this.errors.users = false;
-      this.errors.score = false;
-      this.errors.date = false;
-
-      this.date = new Date();
-    },
-    save() {
-      const date = this.date.toISOString();
-      const score = this.$refs.scored.selected;
-      const out_of = 10;
-      const present = this.usersGroup;
-      this.createPoll({
-        date,
-        score,
-        out_of,
-        present
-      }).then(() => {
-        this.isModalOpen = false;
-        this.$buefy.toast.open({
-          message: "Poll has been recorded"
-        });
-
-        this.reset();
-      });
-    },
-    updatedUsers(users) {
-      this.usersGroup = users;
-    }
-  }
-};
+}
 </script>
--- src/views/Polls.vue
+++ src/views/Polls.vue
@@ -1,164 +1,149 @@
 <template>
   <div class="polls container is-widescreen">
     <chart />
-    <!-- <addPoll /> -->
-    <!-- <section class="pollapp">
-      <header class="header">
-        <h1>Quiz Scores</h1>
-
-      </header>
-      <section class="main" v-show="polls.length" v-cloak>
-        <div class="poll-list">
-          <poll v-for="poll in filteredPolls" :poll="poll" :key="poll._id"/>
-        </div>
-      </section>
-      <footer class="footer" v-show="polls.length" v-cloak>
-    
-      </footer>
-    </section> -->
   </div>
 </template>
 <style scoped>
 </style>
 
 <script>
-import _ from "lodash";
-import vueAddPoll from "@/views/AddPoll.vue"
-import vueChart from "@/components/Chart.vue"
-import vuePoll from "@/views/Poll.vue"
-import PollsMixin from "@/mixins/PollsMixin";
-import UsersMixin from "@/mixins/UsersMixin";
+import _ from 'lodash'
+import vueAddPoll from '@/views/AddPoll.vue'
+import vueChart from '@/components/Chart.vue'
+import vuePoll from '@/views/Poll.vue'
+import PollsMixin from '@/mixins/PollsMixin'
+import UsersMixin from '@/mixins/UsersMixin'
 // visibility filters
 let filters = {
-  all(polls) {
-    return polls;
-  },
-  latest(polls) {
-    return polls.filter(function(poll) {
-      return poll.status === "proposed";
-    });
-  }
-};
+    all(polls) {
+        return polls
+    },
+    latest(polls) {
+        return polls.filter(function(poll) {
+            return poll.status === 'proposed'
+        })
+    },
+}
 
 export default {
-  name: "Polls",
-  mixins: [PollsMixin, UsersMixin],
-  components: {
-    addPoll: vueAddPoll,
-    chart: vueChart,
-    poll: vuePoll
-  },
-  mounted() {
-    // handle routing
-    const onHashChange = () => {
-      let visibility = window.location.hash.replace(/#\/?/, "");
-      if (filters[visibility]) {
-        this.visibility = visibility;
-      } else {
-        window.location.hash = "";
-        this.visibility = "all";
-      }
-    };
-    window.addEventListener("hashchange", onHashChange);
-    onHashChange();
-  },
-  computed: {
-    users() {
-      return this.listUsers;
+    name: 'Polls',
+    mixins: [PollsMixin, UsersMixin],
+    components: {
+        addPoll: vueAddPoll,
+        chart: vueChart,
+        poll: vuePoll,
     },
-    polls() {
-      return this.listPolls;
+    mounted() {
+        // handle routing
+        const onHashChange = () => {
+            let visibility = window.location.hash.replace(/#\/?/, '')
+            if (filters[visibility]) {
+                this.visibility = visibility
+            } else {
+                window.location.hash = ''
+                this.visibility = 'all'
+            }
+        }
+        window.addEventListener('hashchange', onHashChange)
+        onHashChange()
     },
-    currDay() {
-      var dateObj = new Date();
-      var month = dateObj.getUTCMonth() + 1; //months from 1-12
-      var day = dateObj.getUTCDate();
-      var year = dateObj.getUTCFullYear();
-      return Date.parse(year, month, day);
+    computed: {
+        users() {
+            return this.listUsers
+        },
+        polls() {
+            return this.listPolls
+        },
+        currDay() {
+            var dateObj = new Date()
+            var month = dateObj.getUTCMonth() + 1 //months from 1-12
+            var day = dateObj.getUTCDate()
+            var year = dateObj.getUTCFullYear()
+            return Date.parse(year, month, day)
+        },
+        scoreDropdown() {
+            return {
+                options: _.map([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], val => {
+                    return { value: val }
+                }),
+                placeholder: this.currScore,
+                prefix: 'Score',
+                backgroundColor: 'white',
+                textColor: 'black',
+                borderRadius: '0em',
+                border: '1px solid gray',
+            }
+        },
+        userDropdown() {
+            return {
+                options: _.map(this.users, val => {
+                    return { value: val.name }
+                }),
+                placeholder: this.currentUser,
+                prefix: 'Current User:',
+                backgroundColor: 'white',
+                textColor: 'black',
+                borderRadius: '0em',
+                border: '1px solid gray',
+            }
+        },
+        filteredPolls() {
+            return this.listPolls
+        },
     },
-    scoreDropdown() {
-      return {
-        options: _.map([0,1,2,3,4,5,6,7,8,9,10], val => {
-          return { value: val };
-        }),
-        placeholder: this.currScore,
-        prefix: "Score",
-        backgroundColor: "white",
-        textColor: "black",
-        borderRadius: "0em",
-        border: "1px solid gray"
-      };
+    filters: {
+        pluralize(n) {
+            return n === 1 ? 'poll' : 'polls'
+        },
     },
-    userDropdown() {
-      return {
-        options: _.map(this.users, val => {
-          return { value: val.name };
-        }),
-        placeholder: this.currentUser,
-        prefix: "Current User:",
-        backgroundColor: "white",
-        textColor: "black",
-        borderRadius: "0em",
-        border: "1px solid gray"
-      };
-    },
-    filteredPolls() {
-      return this.listPolls;
-    }
-  },
-  filters: {
-    pluralize(n) {
-      return n === 1 ? "poll" : "polls";
-    }
-  },
-  methods: {
-    addPoll() {
-      let value = this.nextDate;
-      if (!value) {
-        return;
-      }
+    methods: {
+        addPoll() {
+            let value = this.nextDate
+            if (!value) {
+                return
+            }
 
-      const { Poll } = this.$FeathersVuex;
-      let poll = new Poll({
-        date: this.nextDate,
-        score: this.score,
-        out_of: this.out_of
-      });
-      poll.save({});
-      this.visibility = "all";
-      this.nextDate = null;
-    },
-    deletePoll(poll) {
-      poll.remove({});
-    },
-    editPoll(poll) {
-      this.beforeEditCache = poll;
-      this.editingDate = poll;
-    },
-    doneEdit(poll) {
-      if (!this.editingDate) {
-        return;
-      }
-      this.editingDate = null;
-      poll.title = poll.title.trim();
+            const { Poll } = this.$FeathersVuex
+            let poll = new Poll({
+                date: this.nextDate,
+                score: this.score,
+                out_of: this.out_of,
+            })
+            poll.save({})
+            this.visibility = 'all'
+            this.nextDate = null
+        },
+        deletePoll(poll) {
+            poll.remove({})
+        },
+        editPoll(poll) {
+            this.beforeEditCache = poll
+            this.editingDate = poll
+        },
+        doneEdit(poll) {
+            if (!this.editingDate) {
+                return
+            }
+            this.editingDate = null
+            poll.title = poll.title.trim()
 
-      poll.save({});
+            poll.save({})
 
-      if (!poll.title) {
-        this.removePoll(poll);
-      }
+            if (!poll.title) {
+                this.removePoll(poll)
+            }
+        },
+        cancelEdit(poll) {
+            this.editingDate = null
+            poll.title = this.beforeEditCache
+        },
+        setScore(selectedOption) {
+            this.currScore = selectedOption.value
+        },
     },
-    cancelEdit(poll) {
-      this.editingDate = null;
-      poll.title = this.beforeEditCache;
-    },
-    setScore(selectedOption) {
-      this.currScore = selectedOption.value;
-    }
-  }
-};
+}
 </script>
 
 <style lang="scss">
-@import "../styles/pollsmvc_app.scss";
+@import '../styles/pollsmvc_app.scss';
 </style>
